abf6bb7f3e3316caa5496e5dda7132627a7a1fab
areYouJay24 <jayspprajapati24@gmail.com>
2025-03-17 11:26:55 -0300

    git cicd test

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const csv = require("csv-parser");
 const app = express();
 app.use(bodyParser.json());
 
+// Test CICD container 2
 
 app.post("/calculate", (req, res) => {
     const { file, product } = req.body;
